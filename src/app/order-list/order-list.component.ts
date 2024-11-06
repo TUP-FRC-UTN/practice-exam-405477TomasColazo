af83ae3a8869630a291ab5b78d0d404f934cb7a6
Tomas Colazo <405477@tecnicatura.frc.edu.ar>
2024-11-06 17:03:07 -0300

    search filter

--- src/app/order-list/order-list.component.ts
+++ src/app/order-list/order-list.component.ts
@@ -2,36 +2,50 @@ import {Component, inject, OnInit} from '@angular/core';
 import {OrderService} from "../services/order.service";
 import {Order} from "../models/order";
 import {DatePipe} from "@angular/common";
-import {routes} from "../app.routes";
 import {Router} from "@angular/router";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-order-list',
   standalone: true,
   imports: [
-    DatePipe
+    DatePipe,
+    FormsModule
   ],
   templateUrl: './order-list.component.html',
   styleUrl: './order-list.component.css'
 })
 export class OrderListComponent implements OnInit {
-private readonly order_service:OrderService = inject(OrderService);
-private readonly router:Router = inject(Router);
-orders:Order[] = [];
+  private readonly order_service:OrderService = inject(OrderService);
+  private readonly router:Router = inject(Router);
+  orders:Order[] = [];
+  filteredOrders: Order[] = [];
+  searchTerm: string = '';
 
 
-ngOnInit(): void {
-  this.getOrders();
+  ngOnInit(): void {
+    this.getOrders();
+    }
+
+  getOrders() {
+    this.order_service.orders.subscribe(orderList => {
+      this.orders = orderList;
+      this.filteredOrders = orderList;
+    })
   }
 
-getOrders() {
-  this.order_service.orders.subscribe(orderList => {
-    this.orders = orderList
-  })
+  filterOrders(){
+    const search = this.searchTerm.toLowerCase().trim();
+    if(!search){
+      this.filteredOrders = this.orders;
+      return;
+    }
+
+    this.filteredOrders = this.orders.filter(order =>
+      order.customerName.toLowerCase().includes(search) ||
+        order.email.toLowerCase().includes(search)
+    );
   }
-
-
-  protected readonly routes = routes;
 
   newOrder() {
     this.router.navigate(['/create-order']);
